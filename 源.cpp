b100c6265521aabd019106de2d275417e45d1472
纯真丁一郎 <iop18653346184@hotmail.com>
2022-12-21 19:09:15 +0800

    添加项目文件。

---
+++ 源.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<graphics.h>
+
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+	initgraph(640, 480);
+	
+	return 0;
+}
